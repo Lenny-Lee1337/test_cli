e5cb48033e2368b36db049323834647488b438c3
Lenny Lee <lennylee91@gmail.com>
2019-12-09 05:10:19 +0000

    Pretty much done

--- lib/test_cli/api.rb
+++ lib/test_cli/api.rb
@@ -1,20 +1,32 @@
 class TestCli::API
   
   def fetch
-    puts "Which berry?"
+   
      url ="https://pokeapi.co/api/v2/berry/"
      response = HTTParty.get(url)
      response["results"].each do |berry|
        name = berry["name"]
        link = berry["url"]
        puts name
-       puts link
        
-       #TestCLI::Berries.new(name, link)
        
+      input = nil
+      while input!= "no"
+        puts "Do you want info on this berry? yes/no"
+        input = gets.strip.downcase
+        case input
+        when "yes"
+          puts link
+          
+        else
+          puts "ok..."
+         
+       end
+      end
+    end
+      
+      
          
      end
-     
+    end
   
-  end
-end
