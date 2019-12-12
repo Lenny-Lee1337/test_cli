236f72ac79e8eda94606a58c238ad96f93bae1ad
Lenny Lee <lennylee91@gmail.com>
2019-12-12 22:04:20 +0000

    Working

--- lib/test_cli/cli.rb
+++ lib/test_cli/cli.rb
@@ -3,7 +3,8 @@ class TestCli::CLI
   def call
     
     TestCli::API.new.fetch
-    #TestCli::Berries.all.each.do |berry|
-      #puts berry.name
-    end
+     TestCli::Berries.all.each do |berry|
+      puts berry.name
+     end
   end
+end
