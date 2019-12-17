165545e97d4d06656f47627ccce8317b239b3300
Lenny Lee <lennylee91@gmail.com>
2019-12-17 02:40:11 +0000

    Fixed if statement

--- lib/test_cli/cli.rb
+++ lib/test_cli/cli.rb
@@ -3,7 +3,7 @@ class TestCli::CLI
   def call
     
     TestCli::API.new.fetch
-     
+     puts "which berry"
      list_berries
      menu
   end
@@ -12,19 +12,20 @@ class TestCli::CLI
     def menu
       input = nil
       while input != "exit"
-       puts "Enter Berry ID:"
+       
         input = gets.strip.downcase
          if input.to_i > 0 && < TestCli::Berries.all.length
            #TODO: Pass user input to a list_details method(not yet created)
        
           puts "Berry's info"
           
+        elsif == "exit"
+          puts "Bye"
         else
-          puts "ok..."
-         
+          puts "Try again"
        end
       end
-  end
+  
   
   def list_berries
     TestCli::Berries.all.each.with_index(1) do |berry, index|
